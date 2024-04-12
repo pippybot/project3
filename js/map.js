75cd4cb9770f0be85e088fd820b8c4e07c2684c0
pippybot <150129081+pippybot@users.noreply.github.com>
2024-04-12 01:42:25 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,36 +1,110 @@
-function init(){
-	//alert('it works');
-	var el = document.getElementById('canvas');
-	var chosenLocation = new google.maps.LatLng(41.83380694415314, -87.62611786373115);
-	var mapOptions = {
-		center: chosenLocation,
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
-	};
+var map;
+var marker;
+const fortworth = {lat: 32.75361817256558, lng: -97.31959193117723};
+const uk = {lat: 55.20928090114521, lng: -2.8708931416767762};
+var curLoc = fortworth;
+var info = 0;
+var layers = 0;
+var transit;
+var traffic;
+var bike;
 
-	var myMap = new google.maps.Map(el, mapOptions);
+function initMap() {
 
-	var marker = new google.maps.Marker({
-		position: chosenLocation,
-		map: myMap,
-		animation: google.maps.Animation.BOUNCE,
-		icon: 'images/lokiHype.PNG'
-	});
+    transit = new google.maps.TransitLayer();
+    traffic = new google.maps.TrafficLayer();
+    bike = new google.maps.BicyclingLayer();
 
-	var contentString = '<h1>IIT Rowe Village</h1><p>Jeanne and John Rowe Village, designed by acclaimed international architect Helmut Jahn, consists of three five-story buildings that face the primary thoroughfare running through Mies Campus. One of its most distinctive features is that the Green Line elevated (“L”) train runs behind, which gives it a distinctly Chicago vibe. This was the building I lived in when I was still on campus! </p>';
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: curLoc,
+        zoom: 10
+    });
 
-	var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  	});
+    // Feature #1:
+    // Bounces one of my Twitch emotes as the marker:
+    marker = new google.maps.Marker({
+        map: map,
+        position: curLoc,
+        animation: google.maps.Animation.BOUNCE,
+        icon: {
+            url: 'images/lokiHype.PNG',
+            scaledSize: new google.maps.Size(50, 50)
+        }
+    });
 
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
-  	});
+    document.getElementById('changeLocButton').addEventListener('click', function() {changeLocation()});
 
+    // Feature #2:
+    document.getElementById('showInfoButton').addEventListener('click', function() {showInfo()});
 
+    // Feature #3:
+    document.getElementById('showLayerButton').addEventListener('click', function() {showLayers()});
+    transit = new google.maps.TransitLayer();
+    traffic = new google.maps.TrafficLayer();
+    bike = new google.maps.BicyclingLayer();
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+function changeLocation() {
+
+    if (curLoc == fortworth) {
+        curLoc = uk;
+        marker.setPosition(uk);
+        map.setCenter(curLoc);
+
+    } else {
+        curLoc = fortworth;
+        marker.setPosition(fortworth);
+        map.setCenter(curLoc);
+    }
+}
+
+function showInfo() {
+
+    if (info == 1) {
+
+        infoWindow.close();
+        info = 0;
+
+    } else {
+
+        if (curLoc == fortworth) {
+
+            infoWindow = new google.maps.InfoWindow({
+                content: "I am going to Fort Worth for my birthday to meet some friends at a gaming event in September.",
+                ariaLabel: "Fort Worth",
+            });
+            
+        } else {
+    
+            infoWindow = new google.maps.InfoWindow({
+                content: "I visted the UK for my first trip outside the United States. My favorite part was Scotland.",
+                ariaLabel: "UK"
+            });
+        }
+
+        infoWindow.open({
+            anchor: marker,
+            map: map,
+        });
+
+        info = 1;
+    }
+}
+
+function showLayers() {
+
+    if (layers == 1) {
+
+        transit.setMap(null);
+        traffic.setMap(null);
+        bike.setMap(null);
+        layers = 0;
+
+    } else {
+        
+        transit.setMap(map);
+        traffic.setMap(map);
+        bike.setMap(map);
+        layers = 1;
+    }
+}
