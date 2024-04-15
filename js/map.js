0127b327af1e6133871722545e894bcfb99b8e66
pippybot <150129081+pippybot@users.noreply.github.com>
2024-04-15 18:57:00 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,51 +1,79 @@
 var map;
+var infoWindow;
+var currentLocation = 'Rowe Village';
+var trafficLayer;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('googleMap'), {
-        center: {lat: 41.83409469359984, lng: -87.62625439108541}, 
-        zoom: 25,
-        mapID: 'Rowe_Village_ID',
-    });
-    
-    new google.maps.Marker({
-        position: {lat: 41.83409469359984, lng: -87.62625439108541},
-        map,
-        animation: google.maps.Animation.BOUNCE,
-        icon: {
-            url: 'images/lokiHype.PNG',
-            scaledSize: new google.maps.Size(50,50)
-        }
+        center: { lat: 41.83409469359984, lng: -87.62625439108541 },  // Rowe Village coordinates
+        zoom: 15  // Adjusted zoom for better initial view
     });
 
-    // Add an event listener to the change location button
-    document.getElementById('changeLocButton').addEventListener('click', changeLocation);
+    trafficLayer = new google.maps.TrafficLayer();
+
+    // Markers for different locations
+    var markers = {
+        'UK': new google.maps.Marker({
+            position: { lat: 55.391223530439646, lng: -2.591264406992628 },
+            map,
+            title: 'UK'
+        }),
+        'Fort Worth': new google.maps.Marker({
+            position: { lat: 32.75361817256558, lng: -97.31959193117723 },
+            map,
+            title: 'Fort Worth'
+        }),
+        'Rowe Village': new google.maps.Marker({
+            position: { lat: 41.83409469359984, lng: -87.62625439108541 },
+            map,
+            title: 'Rowe Village',
+            animation: google.maps.Animation.DROP
+        })
+    };
+
+    // Info window for markers
+    infoWindow = new google.maps.InfoWindow();
+
+    document.getElementById('changeLocButton').addEventListener('click', function() {
+        changeLocation();
+    });
+
+    document.getElementById('showLayerButton').addEventListener('click', function() {
+        toggleLayers();
+    });
+
+    document.getElementById('showInfoButton').addEventListener('click', function() {
+        showInfo(markers[currentLocation]);
+    });
 }
 
-// Marker added for UK 
 function changeLocation() {
-   // Defining coordinates
-   const newLocation = {lat: 55.391223530439646, lng: -2.591264406992628};
-
-    //Set  map's  center to new  location
-    map.setCenter(newLocation);
-}
- 
-    // Marker added for FortWorth
-    function changeLocation() {
-        // Defining coordinates
-        const newLocation = {lat: 32.75361817256558, lng: -97.31959193117723};
-
-    //Set  map's  center to new  location
-    map.setCenter(newLocation);
+    if (currentLocation === 'Fort Worth') {
+        map.setCenter({ lat: 55.391223530439646, lng: -2.591264406992628 });
+        currentLocation = 'UK';
+    } else {
+        map.setCenter({ lat: 32.75361817256558, lng: -97.31959193117723 });
+        currentLocation = 'Fort Worth';
+    }
 }
 
-// Defining the layers
-function layers() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: { lat: 34.04924594193164, lng: -118.24104309082031 },
-  });
-  const trafficLayer = new google.maps.TrafficLayer();
+function toggleLayers() {
+    if (trafficLayer.getMap()) {
+        trafficLayer.setMap(null);
+    } else {
+        trafficLayer.setMap(map);
+    }
+}
 
-  trafficLayer.setMap(map);
+function showInfo(marker) {
+    var contentString = '';
+    if (marker.title === 'UK') {
+        contentString = 'I went to the UK last in Summer of 2023 with my grandmother and my brother.';
+    } else if (marker.title === 'Fort Worth') {
+        contentString = 'This year for my birthday, I am going to a big gaming event for Rocket League.';
+    } else if (marker.title === 'Rowe Village') {
+        contentString = 'When I was studying on campus during the Fall 2023 semester, this is the dorm I stayed at.';
+    }
+    infoWindow.setContent(contentString);
+    infoWindow.open(map, marker);
 }
