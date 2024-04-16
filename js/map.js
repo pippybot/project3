5e497400e25823e9ea55d6194bf7c289f168a021
pippybot <150129081+pippybot@users.noreply.github.com>
2024-04-16 16:41:56 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -78,21 +78,47 @@ function changeLocation() {
 function showLayers() {
 
     if (layers == 1) {
-
         transit.setMap(null);
         traffic.setMap(null);
         bike.setMap(null);
         layers = 0;
-
     } else {
-        
         transit.setMap(map);
         traffic.setMap(map);
         bike.setMap(map);
         layers = 1;
     }
-    
+} 
     // Add an event listener to the change location button
     document.getElementById('showLayerButton').addEventListener('click', showLayers);
-}
 
+
+// Creating the Info button for each marker/place
+var infoButton = document.createElement("button");
+
+// Setting the text for the button
+infoButton.innerText = "Information";
+
+// Attaching the event listener to the button
+button.addEventListener("click", function() {
+});
+
+// Doing the same thing that we did for the markers and location changes, we can do here for the information
+function changeInformation() {
+    let newInfo;
+    // Checking what location we are on to make sure that the correct info pops up (this is for dorm)
+    if (map.getCenter().lat() == locationDorm.lat && map.getCenter().lng() == locationDorm.lng) {
+        // If we ARE at dorm, then the dorm info now alerts
+        alert("This is the dorm I stayed at when I lived on campus in the Fall 2023 semester!");
+    }
+    // Checking if we are on UK
+    else if (map.getCenter().lat() == locationUK.lat && map.getCenter().lng() == locationUK.lng) {
+        // If we ARE at UK, then the UK info now alerts
+        alert("I went to the UK in the summer of 2023 with my brother and my grandmother. It was the first time I had ever been on a plane or out of the country!")
+    }
+    // Checking if we are on Fort Worth
+    else if (map.getCenter().lat() == locationFW.lat && map.getCenter().lng() == locationFW.lng) {
+        // If we ARE at FW, then the FW info now alerts
+        alert("For my birthday this year (2024), I am going to a big gaming event being held at Dikcies Arena for a game I play called 'Rocket League'!")
+    }    
+}
